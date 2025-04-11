705bd48bdbbbb80958285a9aed4c3ef8c6f1bd44
Suvam Singh <singhsupriya711@gmail.com>
2025-04-11 23:30:37 +0530

    Basic Setup

---
+++ src/main/java/com/medicine/medicineinventory/MedicineInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.medicine.medicineinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineInventoryApplication.class, args);
+	}
+
+}
